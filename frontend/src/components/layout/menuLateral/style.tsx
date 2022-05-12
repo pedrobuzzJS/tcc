d612802cc563ce175ea8634b451db1caadfc03e6
Pedro Artub Buzzi Pereira <pedro.buzzi@unidavi.edu.br>
2022-05-12 20:13:30 -0300

    0

--- frontend/src/components/layout/menuLateral/style.tsx
+++ frontend/src/components/layout/menuLateral/style.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 export const MenuLateral = styled.section`
     height: 65px;
     display: flex;
-    backgound-color: #1A202C;
-    boxShadow: 0 0 20px 3px;
+    background-color: #1A202C;
+    box-shadow: 0 0 20px 3px;
 `;
 
 export default MenuLateral;
