be853337b30ce3d3bb95766ec9bb0e8a9dab0fc5
Pedro Artub Buzzi Pereira <pedro.buzzi@unidavi.edu.br>
2022-05-18 23:51:46 -0300

    a

--- frontend/src/components/Menu/styles.ts
+++ frontend/src/components/Menu/styles.ts
@@ -2,4 +2,8 @@ import styled from "styled-components";
 
 export const Container = styled.div``;
 
+export const Menu = styled.nav``;
+
 export const ListaMenu = styled.ul``;
+
+export const ItemMenu = styled.li``;
--- frontend/src/styles/GlobalStyles.ts
+++ frontend/src/styles/GlobalStyles.ts
@@ -9,6 +9,7 @@ export default createGlobalStyle`
 
     html, body, #root {
         height: 100%;
+        background-color: #eee;
     }
 
     *, button, input {
